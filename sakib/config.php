668863dfe9145341bff27483aee71fbcb1883e5d
MD Sakib Al Hasan <iamsakib360@gmail.com>
2026-03-20 19:08:59 +0600

    Delete sakib directory

--- sakib/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "ai_chatbot");
-
-define("GEMINI_API_KEY", "YOUR_GEMINI_API_KEY_HERE");
-?>
